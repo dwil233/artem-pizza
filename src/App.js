26343d1be50220e741737956294fb3e5ab437a9c
dwil233 <d.ilyushenko@gmail.com>
2020-11-10 16:02:05 +0700

    Delete forgotten comment

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@ import {Configurator} from './Configurator';
 export function App() {
   return <div className="container">
     <Configurator/>
-    {/*<form>*/}
-    {/*  <div className="radio">*/}
-    {/*    <label>*/}
-    {/*      <input type="radio" value="option1" checked={true} name="pizzaSize"/>*/}
-    {/*      Option 1*/}
-    {/*    </label>*/}
-    {/*  </div>*/}
-    {/*  <div className="radio">*/}
-    {/*    <label>*/}
-    {/*      <input type="radio" value="option2" name="pizzaSize" />*/}
-    {/*      Option 2*/}
-    {/*    </label>*/}
-    {/*  </div>*/}
-    {/*  <div className="radio">*/}
-    {/*    <label>*/}
-    {/*      <input type="radio" value="option3" name="pizzaSize" />*/}
-    {/*      Option 3*/}
-    {/*    </label>*/}
-    {/*  </div>*/}
-    {/*</form>*/}
   </div>
 }
 
